fd162a296a5940451dd71497c214aeaee59a12e2
Frederic Gingras <frederic@gingras.cc>
2014-01-28 18:06:58 +0000

    Quite a bit done!

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
+require 'pivotal-tracker'
+require 'securerandom'
+require './pdf'
 
+enable :sessions
 set :sessions, true
 set :views, Proc.new { File.join(root, "views") }
 set :bind, '0.0.0.0'
 set :port, 3000
-
+    
 error do
   erb :'500'
 end
@@ -14,5 +18,31 @@ get '/' do
 end
 
 post '/' do
-  params[:apikey] 
+  session[:apikey] = params[:apikey]
+  PivotalTracker::Client.token = session[:apikey]
+  @projects = PivotalTracker::Project.all
+  erb :projects, :locals => { :projects => @projects }
+end
+
+post '/print_label' do
+  pid = params[:id]
+  lid = params[:label_id]
+  PivotalTracker::Client.token = session[:apikey]
+  @project = PivotalTracker::Project.find(pid.to_i)
+  logger.info(@project.inspect)
+  @stories = @project.stories.all(:filter => 'label:"'+lid+'"')
+  logger.info(@stories.inspect)
+  filename = 'pivotal_cards_'+SecureRandom.hex+'.pdf'
+  #headers["Content-Type"] = "Application/octet-stream"
+  headers["Content-Disposition"] = "attachment;filename=" + filename
+  get_cards_file(@stories)
+  #send_file get_cards_file(@stories), :filename => filename, :type => 'Application/octet-stream'
+end
+
+post '/print_iteration' do
+  pid = params[:id]
+  iid = params[:iteration_id]
+  PivotalTracker::Client.token = session[:apikey]
+  @project = PivotalTracker::Project.find(pid)
+  
 end
--- pdf.rb
+++ pdf.rb
@@ -16,121 +16,60 @@ require 'prawn/layout/grid'
 
 class String; include Term::ANSIColor; end
 
-file = ARGV.first
-
-unless file
-  puts "[!] Please provide a path to CSV file"
-  exit 1
-end
-
-# --- Read the CSV file -------------------------------------------------------
-
-stories = CSV.read(file)
-headers = stories.shift
-
-# p headers
-# p stories
-
-# --- Hold story in Card class
-
-class Card < OpenStruct
-  def type
-    @table[:type]
-  end
-end
-
-# --- Create cards objects
-
-cards = stories.map do |story|
-  attrs =  { :title  => story[1]   || '',
-             :body   => story[14]  || '',
-             :type   => story[6]   || '',
-             :points => story[7]   || '...',
-             :owner  => story[13]  || '.'*50}
-
-  Card.new attrs
-end
-
-# p cards
-
-# --- Generate PDF with Prawn & Prawn::Document::Grid
-
-begin
-
-outfile = File.basename(file, ".csv")
-
-Prawn::Document.generate("#{outfile}.pdf",
-   :page_layout => :landscape,
-   :margin      => [25, 25, 50, 25],
-   :page_size   => 'A4') do |pdf|
-
-    @num_cards_on_page = 0
-
-     pdf.font "Courier"
-
-    cards.each_with_index do |card, i|
-
-      # --- Split pages
-      if i > 0 and i % 4 == 0
-        # puts "New page..."
-        pdf.start_new_page
-        @num_cards_on_page  = 1
-      else
-        @num_cards_on_page += 1
-      end
-
-      # --- Define 2x2 grid
-      pdf.define_grid(:columns => 2, :rows => 2, :gutter => 42)
-      # pdf.grid.show_all
-
-      row    = (@num_cards_on_page+1) / 4
-      column = i % 2
-
-      # p @num_cards_on_page
-      # p [ row, column ]
-
-      padding = 12
-
-      cell = pdf.grid( row, column )
-      cell.bounding_box do
-
-        pdf.stroke_color = "666666"
-        pdf.stroke_bounds
-
-        # --- Write content
-        pdf.bounding_box [pdf.bounds.left+padding, pdf.bounds.top-padding], :width => cell.width-padding*2 do
-          pdf.text card.title, :size => 14
-          pdf.text "\n", :size => 14
-          pdf.fill_color "444444"
-          pdf.text card.body, :size => 10
-          pdf.fill_color "000000"
-        end
-
-        pdf.text_box "Points: " + card.points,
-          :size => 12, :at => [12, 50], :width => cell.width-18
-        pdf.text_box "Owner: " + card.owner,
-          :size => 8, :at => [12, 18], :width => cell.width-18
-
-        pdf.fill_color "999999"
-        pdf.text_box card.type.capitalize,  :size => 8,  :align => :right, :at => [12, 18], :width => cell.width-18
-        pdf.fill_color "000000"
-
-      end
-
+def get_cards_file(cards)
+  pdf = Prawn::Document.new({
+      :page_layout => :landscape,
+      :margin      => [25, 25, 50, 25],
+      :page_size   => 'A4'
+    })
+  
+  @num_cards_on_page = 0
+  pdf.font "Courier"
+  
+  cards.each_with_index do |card, i|
+    # --- Split pages
+    if i > 0 and i % 4 == 0
+      pdf.start_new_page
+      @num_cards_on_page  = 1
+    else
+      @num_cards_on_page += 1
     end
+    
+    # --- Define 2x2 grid
+    pdf.define_grid(:columns => 2, :rows => 2, :gutter => 42)
+    
+    row    = (@num_cards_on_page+1) / 4
+    column = i % 2
+    padding = 12
 
-    # --- Footer
-    #pdf.number_pages "#{outfile}.pdf", [pdf.bounds.left,  -28]
-    #pdf.number_pages "<page>/<total>", [pdf.bounds.right-16, -28]
-end
+    cell = pdf.grid( row, column )
+    cell.bounding_box do
 
-puts ">>> Generated PDF file in '#{outfile}.pdf' with #{cards.size} stories:".black.on_green
-
-cards.each do |card|
-  puts "* #{card.title}"
-end
-
-rescue Exception
-  puts "[!] There was an error while generating the PDF file... What happened was:".white.on_red
-  raise
+      pdf.stroke_color = "666666"
+      pdf.stroke_bounds
+      
+      # --- Write content
+      pdf.bounding_box [pdf.bounds.left+padding, pdf.bounds.top-padding], :width => cell.width-padding*2 do
+        pdf.text card.name, :size => 14
+        pdf.text "\n", :size => 14
+        pdf.fill_color "444444"
+        pdf.text card.description, :size => 10
+        pdf.fill_color "000000"
+      end
+      
+      pdf.text_box "Points: " + card.estimate.to_s,
+      :size => 12, :at => [12, 50], :width => cell.width-18
+      pdf.text_box "Labels: " + card.labels,
+      :size => 8, :at => [12, 18], :width => cell.width-18
+      
+      pdf.fill_color "999999"
+      pdf.text_box card.story_type.capitalize,  :size => 8,  :align => :right, :at => [12, 18], :width => cell.width-18
+      pdf.fill_color "000000"
+      
+    end
+    
+  end # each cards
+  pdf.number_pages "<page>/<total>", {:align => :center}
+  
+  pdf.render
 end
